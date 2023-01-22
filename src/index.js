ac40d6f42406d6485ec3731570b5a1c7b2bbaa20
Aroop Kumar <aroopkumar50@gmail.com>
2023-01-22 06:44:59 +0530

    Add socket.io module to project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
+io.on("connection", () => {
+    console.log("New WebSocket connection!");
+});
+
+server.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
 });
